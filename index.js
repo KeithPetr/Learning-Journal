ddd42e51b7ff006154c07a5262ed1aa2a3130e4f
KeithPetr <keith.petryshyn@gmail.com>
2023-02-14 11:51:52 -0500

    View More button works
    It displays the remaining posts and changes between 'more' and 'less' when clicked on

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
-import { blogArray } from "./data.js";
-
-const mainContentEl = document.getElementById("main-content");
-const blogClasses = document.getElementsByClassName("blog-post");
+const blogPosts = document.querySelectorAll(".blog-post");
+const viewMoreBtn = document.getElementById("view-more-btn");
 
 
-// the below function takes in the blog array from the data.js file.
-// it then looks at the width of the browser window to determine how
-// big the width is for viewing. based on the size, the function then
-// determines how many blog posts should be rendered.
 
-function renderBlogArray() {
-  let innerWidth = window.innerWidth;
-  let blogString = "";
-  if (innerWidth < 481) {
-    for (let i = 0; i < 3; i++) {
-      blogString += blogArray[i];
-    }
-  } else if (innerWidth >= 481 && innerWidth <= 718) {
-    for (let i = 0; i < 4; i++) {
-      blogString += blogArray[i];
-    }
-  } else if (innerWidth >= 719 && innerWidth <= 964) {
-    for (let i = 0; i < 5; i++) {
-      blogString += blogArray[i];
-    }
-  } else if (innerWidth >= 965) {
-    for (let i = 0; i < 6; i++) {
-      blogString += blogArray[i];
-    }
+function viewMoreBlogs() {
+    for (let blog of blogPosts) {
+     blog.classList.toggle("display-post");
   }
-  addFlexChange();
-  mainContentEl.innerHTML = blogString;
+  if (viewMoreBtn.innerText === "View More") {
+    viewMoreBtn.innerText = "View Less"
+   } else if (viewMoreBtn.innerText === "View Less") {
+    viewMoreBtn.innerText = "View More"
+   }
 }
 
-// the below function is an attempt to make the blog posts bigger
-// on very wide screens. It changes the flex-basis of the blog posts
-// by adding a class to those elements.
-
-function addFlexChange() {
-  for (let i = 0; i < blogClasses.length; i++) {
-    if (innerWidth >= 965) {
-      blogClasses[i].classList.add("flex-change");
-    } else {
-      blogClasses[i].classList.remove("flex-change");
-    }
-  }
-}
-
-window.addEventListener("resize", renderBlogArray);
-window.addEventListener("load", renderBlogArray);
-window.addEventListener("resize", addFlexChange);
-window.addEventListener("load", addFlexChange);
-
+viewMoreBtn.addEventListener("click", viewMoreBlogs)
